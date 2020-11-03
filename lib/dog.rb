769af3bbfe8b99430d268e57f608a57083395a69
Devin Wilson <craigvin84@yahoo.com>
2020-11-03 23:25:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initilalize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_all
+    self.all.each do |mutt|
+      print mutt
+    end
